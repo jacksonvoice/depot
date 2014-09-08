66ac1d4df4d3216a66a41ddcac53002b96c3db03
Daniel Jackson <jacksonvoice@yahoo.com>
2014-09-08 12:56:50 -0700

    updated seed data so there are three entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,22 @@ Product.create!(title: 'Programming Ruby 1.9 & 2.0',
   image_url: 'ruby.jpg',
   price: 49.95)
 #...
+Product.create!(title: 'Rails testing',
+  description:
+    %{<p>
+        Ruby is the fastest growing and most exciting dynamic language
+        out there. If you need to get working programs delivered fast,
+        you should add Ruby to your toolbox.
+      </p>},
+  image_url: 'rtp.jpg',
+  price: 49.95)
+
+Product.create!(title: 'Coffee Script',
+  description:
+    %{<p>
+        Ruby is the fastest growing and most exciting dynamic language
+        out there. If you need to get working programs delivered fast,
+        you should add Ruby to your toolbox.
+      </p>},
+  image_url: 'cs.jpg',
+  price: 49.95)
