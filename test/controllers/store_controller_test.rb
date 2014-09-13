4f04301b4af2dda13b8b8d1120317a849a117d21
Daniel Jackson <jacksonvoice@yahoo.com>
2014-09-12 19:20:22 -0700

    added test to check that store index page was working and loading fixtures

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -6,4 +6,14 @@ class StoreControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+
+  test "should show store index page layout" do 
+  	get :index
+  	assert_select 'tr.store-index-list', minimum: 3
+  	assert_select 'h3', 'Programing Ruby 1.9'
+  	assert_select '.price', /\$[,\d]+\.\d\d/
+  end
+
+
+
 end
